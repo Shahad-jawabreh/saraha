b95834718db028b776d1e29cdb52119ace80110c
shahad-jawabreh <s12028521@stu.najah.edu>
2024-04-29 21:59:56 +0300

    upadte

--- appInit.js
+++ appInit.js
@@ -2,8 +2,10 @@ import  connection  from './DB/connection.js';
 import massegeRouter from './src/modules/massege/massege.router.js'
 import authRouter from './src/modules/auth/auth.router.js'
 import userRouter from './src/modules/user/user.router.js'
+import  cors from 'cors'
 const appInit = (app,express)=>{
     connection()
+    app.use(cors())
     app.use(express.json());
     app.get('/',(req,res)=>{return res.json({massege : "welcom!"})})
     app.use('/user',userRouter)
